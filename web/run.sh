821dd77bfd098a1175feed4005ec748f6eadf198
delta114514 <gey3933@gmail.com>
2022-03-18 05:59:03 +0900

    test

--- web/run.sh
+++ web/run.sh
@@ -2,8 +2,9 @@
 
 cd /hugo || exit
 
-wget -O - "https://api.github.com/users/nagataaaas/repos?type=owner&sort=updated&per_page=100" --no-check-certificate >/hugo/static/github_repos.json
-echo cron starting
-hugo -D -d /var/www/html
-cron -f
-# hugo server --bind 0.0.0.0 --port 80 --disableLiveReload -b "/"
+if wget -O - "https://api.github.com/users/nagataaaas/repos?type=owner&sort=updated&per_page=100" --no-check-certificate >/github_repos.tmp; then
+  cp /github_repos.tmp /hugo/static/github_repos.json
+  /usr/local/bin/hugo -D -d /hugo/public
+fi
+
+rm /github_repos.tmp
