2b5012bd67c2bf6cd080e1f31fae33341b5d9cae
delta114514 <gey3933@gmail.com>
2022-03-18 06:11:07 +0900

    test

--- web/run.sh
+++ web/run.sh
@@ -2,9 +2,11 @@
 
 cd /hugo || exit
 
-if wget -O - "https://api.github.com/users/nagataaaas/repos?type=owner&sort=updated&per_page=100" --no-check-certificate >/github_repos.tmp; then
+if wget -O - "https://api.github.com/users/nagataaaas/repos?type=owner&sort=updated&per_page=100" --no-check-certificate -q >/github_repos.tmp; then
   cp /github_repos.tmp /hugo/static/github_repos.json
   /usr/local/bin/hugo -D -d /hugo/public
+  echo refreshed
 fi
 
 rm /github_repos.tmp
+echo finished
