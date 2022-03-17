fcebc5182a3cf07075be5f99faaf68c66d9afcc4
delta114514 <gey3933@gmail.com>
2022-03-18 04:10:45 +0900

    update submodule

--- web/run.sh
+++ web/run.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
-cd /
-if hugo new site hugo -f yml; then
-  echo "created hugo site"
-  cd /hugo || exit
-  echo theme: "PaperMod" >>config.yml
-else
-  echo "site is already exist"
-  cd /hugo || exit
-fi
 
-mv /themes /hugo/ -f
+cd /hugo || exit
 
 wget -O - "https://api.github.com/users/nagataaaas/repos?type=owner&sort=updated&per_page=100" --no-check-certificate >/hugo/static/github_repos.json
 echo cron starting
