98152afc9ee274643078eb0b6e1e4faa96107096
delta114514 <gey3933@gmail.com>
2022-03-18 05:00:56 +0900

    test

--- web/run.sh
+++ web/run.sh
@@ -4,6 +4,6 @@ cd /hugo || exit
 
 wget -O - "https://api.github.com/users/nagataaaas/repos?type=owner&sort=updated&per_page=100" --no-check-certificate >/hugo/static/github_repos.json
 echo cron starting
-cron -f &
-echo cron started
-hugo server --bind 0.0.0.0 --port 80 --disableLiveReload -b "https://nagata.pro"
+hugo -D -d /var/www/html
+cron -f
+# hugo server --bind 0.0.0.0 --port 80 --disableLiveReload -b "/"
