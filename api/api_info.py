7a403fa05abebb27593935d3389c81e3579ffa2c
delta114514 <gey3933@gmail.com>
2022-03-22 03:37:12 +0900

    add cors and change contact

--- api/api_info.py
+++ api/api_info.py
@@ -4,5 +4,6 @@ about = {'title': 'API for nagata.pro',
 This is a Useful utility for developers.
 See docs for more information.
 
-docs at [https://api.nagata.pro/docs](https://api.nagata.pro/docs)''',
-         'contact': 'contact@nagata.pro'}
+docs at [https://api.nagata.pro/docs](https://api.nagata.pro/docs)
+
+contact at nagata@nagata.pro'''}
